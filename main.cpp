41c1a4252376fb2b74b31c81f632d5bc237c90d4
ymorales-galvan <ymorales-galvan@mail.csuchico.edu>
2019-10-02 19:04:35 -0700

    added number of lines in file and number of characters

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ int main()
   string fileName; //to get the name of the file to open
   string line; //to store a single line of a text file
   fstream fileStream; //to open a file for reading
+  int charactersCounted; //number of character in line
+  int linesCounted = 0; //number of lines in file 
+   
 
   cout<<"What file do you want to open? ";
   getline(cin, fileName);
@@ -16,31 +19,40 @@ int main()
   //STEP 1: open the fileStream for input, using the fileName specified
  
  fileStream.open(fileName.c_str(), ios::in);
-  
+
+
 //  if( /*STEP 2: check to see if the fileStream successfully opened*/ )
 if ( fileStream.is_open() )
   {
     cout<<fileName<<" opened.\nFILE CONTENTS:\n";
      
-    //STEP 3: repeat the following until the end-of-file (eof) has been reached...
+   
+//STEP 3: repeat the following until the end-of-file (eof) has been reached...
 while (!fileStream.eof() )
 {
-    // 3A: read a line from fileStream into the variable line
+// 3A: read a line from fileStream into the variable line
 getline( fileStream, line) ;
 
-    // 3B: display the line, followed by an endline
+// 3B: display the line, followed by an endline
+if (!line.empty()) 
+{
+charactersCounted = line.length();
+linesCounted +=1; 
+cout<< line<<endl;
 
+}
 
-cout<< line <<endl;
   
     //STEP 4: close the fileStream
-  }
+ 
 fileStream.close();
 }
-  else
+}
+else
   {
     cout<<fileName<<" could not be opened.\n";
   }
+cout<< "METADATA\n" <<"File:"<<fileName<< "\n" <<"Lines:"<<linesCounted<< "\n" <<"Character"<<charactersCounted<< "\n"; 
 
   return 0;
 }
