7b7aa0d8fcce34408aadd054e3b5b07b72449702
ymorales-galvan <ymorales-galvan@mail.csuchico.edu>
2019-10-02 21:52:10 -0700

    asked the user if they would like to open another file

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
   fstream fileStream; //to open a file for reading
   int charactersCounted; //number of character in line
   int linesCounted = 0; //number of lines in file 
-   
+ char response;   
 
   cout<<"What file do you want to open? ";
   getline(cin, fileName);
@@ -34,9 +34,9 @@ while (!fileStream.eof() )
 getline( fileStream, line) ;
 
 // 3B: display the line, followed by an endline
-if  (!line.empty()) 
+if  (!line.empty()) ;
 {
-charactersCounted = line.length();
+charactersCounted = line.length() + charactersCounted;
 linesCounted +=1; 
 cout<< line<<endl;
 
@@ -54,5 +54,57 @@ else
   }
 cout<< "METADATA\n" <<"File: " <<fileName<< "\n" <<"Lines: " <<linesCounted<< "\n" <<"Characters: "<<charactersCounted<< "\n"; 
 
+
+cout<< "Analyze another file (y/n)?";
+cin>> response;
+
+do {
+
+  cout<<"What file do you want to open? ";
+  getline(cin, fileName);
+
+  //STEP 1: open the fileStream for input, using the fileName specified
+
+ fileStream.open(fileName.c_str(), ios::in);
+
+
+//  if( /*STEP 2: check to see if the fileStream successfully opened*/ )
+if ( fileStream.is_open() )
+  {
+    cout<<fileName<<" opened.\nFILE CONTENTS:\n";
+
+
+//STEP 3: repeat the following until the end-of-file (eof) has been reached...
+while (!fileStream.eof() )
+{
+// 3A: read a line from fileStream into the variable line
+getline( fileStream, line) ;
+
+// 3B: display the line, followed by an endline
+if  (!line.empty()) ;
+{
+charactersCounted = line.length() + charactersCounted;
+linesCounted +=1;
+cout<< line<<endl;
+
+}
+
+
+    //STEP 4: close the fileStream
+
+fileStream.close();
+}
+}
+else
+  {
+    cout<<fileName<<" could not be opened.\n";
+  }
+cout<< "METADATA\n" <<"File: " <<fileName<< "\n" <<"Lines: " <<linesCounted<< "\n" <<"Characters: "<<charactersCounted<< "\n"; 
+
+cout<< "Analyze another file (y/n)?";
+cin>> response;
+}
+
+while (response == 'y'|| response == 'Y'); 
   return 0;
 }
