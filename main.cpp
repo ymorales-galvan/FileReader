cfa4ceb022af03652c678597e893ae2f827cb2f0
ymorales-galvan <ymorales-galvan@mail.csuchico.edu>
2019-10-03 12:27:37 -0700

    added an option to open file again

--- main.cpp
+++ main.cpp
@@ -6,63 +6,65 @@ using namespace std;
 
 int main()
 {
-char response;
-do {
+        char response = 'y';
+        do {
 
-  string fileName; //to get the name of the file to open
-  string line; //to store a single line of a text file
-  fstream fileStream; //to open a file for reading
-  int charactersCounted = 0; //number of character in line
-  int linesCounted = 0; //number of lines in file 
-
-
-  cout<<"What file do you want to open? ";
-  getline(cin, fileName);
-  
-  //STEP 1: open the fileStream for input, using the fileName specified
- 
- fileStream.open(fileName.c_str(), ios::in);
+                string fileName; //to get the name of the file to open
+                string line; //to store a single line of a text file
+                fstream fileStream; //to open a file for reading
+                int charactersCounted = 0; //number of character in line
+                int linesCounted = 0; //number of lines in file 
 
 
-//  if( /*STEP 2: check to see if the fileStream successfully opened*/ )
-if ( fileStream.is_open() )
-  {
-    cout<<fileName<<" opened.\nFILE CONTENTS:\n";
-     
-   
-//STEP 3: repeat the following until the end-of-file (eof) has been reached...
-while (!fileStream.eof() )
-{
-// 3A: read a line from fileStream into the variable line
-getline( fileStream, line) ;
+                cout<<"What file do you want to open? ";
+                getline(cin, fileName);
+                cout<< "file name" ;
 
-// 3B: display the line, followed by an endline
-if  (!line.empty()) 
-{
-charactersCounted = charactersCounted + line.length();
-linesCounted = linesCounted + 1; 
-cout<< line<<endl;
+                //STEP 1: open the fileStream for input, using the fileName specified
 
-}
-
-  
-    //STEP 4: close the fileStream
- 
-}
-}
-else
-  {
-    cout<<fileName<<" could not be opened.\n";
-  } 
-cout<< "METADATA\n" <<"File: " <<fileName<< "\n" <<"Lines: " <<linesCounted<< "\n" <<"Characters: "<<charactersCounted<< "\n"; 
+                fileStream.open(fileName.c_str(), ios::in);
 
 
-cout<< "Analyze another file (y/n)? ";
-cin>> response;
+                //  if( /*STEP 2: check to see if the fileStream successfully opened*/ )
+                if ( fileStream.is_open() )
+                {
+                        cout<<fileName<<" opened.\nFILE CONTENTS:\n";
 
-}
 
-while (response == 'y'|| response == 'Y'); 
+                        //STEP 3: repeat the following until the end-of-file (eof) has been reached...
+                        while (!fileStream.eof() )
+                        {
+                                // 3A: read a line from fileStream into the variable line
+                                getline( fileStream, line) ;
 
-return 0;
+                                // 3B: display the line, followed by an endline
+                                if  (!line.empty()) 
+                                {
+                                        charactersCounted = charactersCounted + line.length();
+                                        linesCounted = linesCounted + 1; 
+                                        cout<< line<<endl;
+
+                                }
+
+
+                                //STEP 4: close the fileStream
+
+                        }
+                }
+                else
+                {
+                        cout<<fileName<<" could not be opened.\n";
+                } 
+                cout<< "METADATA\n" <<"File: " <<fileName<< "\n" <<"Lines: " <<linesCounted<< "\n" <<"Characters: "<<charactersCounted<< "\n"; 
+
+
+                cout<< "Analyze another file (y/n)? ";
+                cin>> response;
+                cin.ignore();
+
+        }
+
+        while (response == 'y'|| response == 'Y'); 
+
+        return 0;
 }
